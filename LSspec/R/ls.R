3d965f8e0a213489d89480f21dd6fcb9a77468b1
Aarya Patil <aaryapatil@Aaryas-MacBook-Pro-2.local>
2020-11-05 12:50:49 -0500

    MTLS v1.0

--- LSspec/R/ls.R
+++ LSspec/R/ls.R
@@ -2,40 +2,6 @@
 # computing the lomb-scargle periodogram
 # using the nfft
 
-PspecMT <- function( t, x, os = 1, tRange = range(t), w, k, tpI = NULL, subtract.mean = TRUE ){
-  # Multitapering of the Schuster periodogram
-  if( is.null(tpI) ) tpI <- dpssApproxFun( t, w, k, tRange = tRange )
-  if( subtract.mean ) x <- x - mean(x)
-  T <- diff( tRange ); N <- length(t)
-  
-  # The interpolated tapers
-  tapers <- lapply( tpI, do.call, list(t) )
-  # The tapers * data
-  pf <- function( taper, t, x, T, dt ){
-    taper/sum(taper^2) * x * sqrt( T/dt ) 
-  }
-  tpx <- lapply( tapers, pf, t = t, x = x, T = T, dt = T/N )
-  
-  # Compute nfft
-  nfft.2 <- function( x, t, os, setN, tRange ) nfft( t, x, os = os, setN = TRUE, tRange = tRange )
-  spc <- lapply( tpx, nfft.2, t = t, os = os, tRange = tRange )
-  freq <- seq(from = 0, by = 1/os/diff(tRange), along.with = spc[[1]] )
-  Pall <- sapply( spc, function(x) abs(x)^2 )
-  
-  # Averaging to get multitaper statistic
-  # Note that we multiply by deltat * oversample
-  out <- data.frame( freq = freq, P = rowMeans(Pall)* 0.5 * T / N / N )
-  attr( out, "w" ) <- w
-  attr( out, "k" ) <- k
-  attr( out, "n" ) <- N
-  attr( out, "tRange" ) <- tRange
-  attr( out, "os" ) <- os
-  attr( out, "subtract.mean" ) <- subtract.mean
-  out
-}
-
-
-
 LSspec <- function( x, t, os = 1, tRange = range(t) ){
 
   # compute nfft of irregular time series
@@ -95,45 +61,269 @@ dpssApproxFun <- function( t, w, k, tRange = range(t) ){
 out
 }
 
-
-
-LSspecMT <- function( t, x, os = 1, tRange = range(t), w, k, tpI = NULL, subtract.mean = TRUE ){
-  # Multitapering of the Lomb-Scargle periodogram
-  if( is.null(tpI) ) tpI <- dpssApproxFun( t, w, k, tRange = tRange )
-  if( subtract.mean ) x <- x - mean(x)
-  T <- diff( tRange ); N <- length(t)
+dpssApproxFunEigen <- function(t, w, k, tRange = range(t), returnEigenvalues = TRUE){
+  # library required for dpss
+  require(multitaper)
   
-  # The interpolated tapers * data
-  # pf <- function( obj, t, x, T, dt ){
-  #   taper <- predict( obj, t )$y
-  #   # renormalize
-  #   taper/sum(taper^2) * (x - mean(x)) * sqrt( T/dt ) 
-  # }
-  # tpx <- lapply( tpI, pf, t = t, x = x, T = T, dt = T/N )
-
-  # The interpolated tapers
-  tapers <- lapply( tpI, do.call, list(t) )
-  # The tapers * data
-  pf <- function( taper, t, x, T, dt ){
-    taper/sum(taper^2) * x * sqrt( T/dt ) 
+  # number of samples
+  n <- length(t)
+  
+  # Spline Functions to interpolate DPSS tapers
+  tp <- dpss(n = n, k = k, nw = n * w, returnEigenvalues = returnEigenvalues)
+  tpVal <- as.data.frame(tp$v)
+  splF <- function(taper, t) splinefun(t, taper)
+  tpI <- lapply(tpVal, splF, t = seq(from = tRange[1], to = tRange[2],
+                                     length.out = n))
+  out <- list(tpI = tpI)
+  
+  # Return eigenvalues
+  if(returnEigenvalues){
+    out$eigen <- tp$eigen
   }
-  tpx <- lapply( tapers, pf, t = t, x = x, T = T, dt = T/N )
+  attr(out, "w") <- w
+  attr(out, "k") <- k
+  attr(out, "n") <- n
+  attr(out, "tRange") <- tRange
+  out
+}
+
+Ftestfun <- function(t, k, x=NULL, w=NULL, tpI = NULL, preprocess = NULL, tpIVal = NULL, freq = NULL, powerK = NULL, tRange = range(t)){
+  # Calculate Delta t
+  N <- length(t)
+  T <- diff(tRange)
+  deltat <- T/N
+  
+  if(is.null(preprocess)){
+    # Compute Interpolated DPSS tapers if not provided
+    if(is.null(tpI))
+      tpI <- dpssApproxFun(t = t, w = w, k = k, tRange = tRange)
+    
+    tpIVal <- lapply(tpI, do.call, list(t))
+    
+    # The tapers * data
+    pf <- function(taper, t, x, T, dt){
+      taper/sum(taper^2) * x * sqrt(T/dt)
+    }
+    taperedData <- lapply(tpIVal, pf, t = t, x = x, T = T, dt = deltat)
+    
+    # Compute spectra
+    spc <- lapply(taperedData, LSspec, t = t, tRange = tRange)
+    
+    # Frequency and power
+    freq <- spc[[1]]$freq
+    powerK <- sapply(spc, getElement, name = "P")
+  }
+  else{
+    if(is.null(tpIVal) | is.null(freq) | is.null(powerK)){
+      stop("Values of tapers and Spectrum Estimate must be provided.")
+    }
+  }
+  
+  tpIVal <- data.frame(tpIVal)
+  tpIVal <- tpIVal*sqrt(deltat)
+  Uk0 <- apply(tpIVal, 2, sum)    ## Percival and Walden H0
+  
+  ## Even tapers are symmetric and hence their summation goes to 0
+  if(k >= 2) {
+    Uk0[seq(2,k,2)] <- 0
+  }
+  
+  # H0 sum of squares
+  Uk0SumSq <- as.numeric(t(Uk0)%*%Uk0)
+  
+  # Mean estimate of the amplitude of the periodic signal
+  MuEstimate <- (powerK %*% Uk0) /Uk0SumSq
+  # Variance explained by the presence of the periodic signal in the data
+  SigVar <-  (Mod(MuEstimate)^2)*Uk0SumSq
+  
+  # Variance of residual coloured noise
+  Uk0 <- as.matrix(Uk0)
+  NoiseVar <- apply(Mod(powerK - (MuEstimate %*% t(Uk0)))^2,
+                    1, sum)
+  # k-1 degrees of freedom
+  F_ <- (k-1)*SigVar/NoiseVar
+  out <- data.frame(F_ = F_, freq = freq)
+  out
+}
+
+LSspecMT <- function(t, x, os = 1, tRange = range(t), w, k, tpI = NULL, subtract.mean = TRUE, niterations = NULL, Ftest = TRUE){
+  # Number of samples
+  N <- length(t)
+  # Total time window
+  T <- diff(tRange)
+  
+  if(subtract.mean) x <- x - mean(x)
+  
+  # Compute Interpolated DPSS tapers if not provided
+  if(is.null(tpI)){
+    dpssIN <- dpssApproxFunEigen(t, w, k, tRange = tRange, returnEigenvalues = TRUE)
+    tpI <- dpssIN$tpI
+    lambdaK <- dpssIN$eigen    # Eigenvalues are required only if adaptive weighting is performed
+  }
+  
+  tpIVal <- lapply(tpI, do.call, list(t))
+  
+  # The tapers * data
+  pf <- function(taper, t, x, T, dt){
+    taper/sum(taper^2) * x * sqrt(T/dt) 
+  }
+  taperedData <- lapply(tpIVal, pf, t = t, x = x, T = T, dt = T/N)
   
   # Compute spectra
-  spc <- lapply( tpx, LSspec, t = t, os = os, tRange = tRange )
+  spc <- lapply(taperedData, LSspec, t = t, os = os, tRange = tRange )
+  # Frequency and power
   freq <- spc[[1]]$freq
-  Pall <- sapply( spc, getElement, name = "P" )
+  powerK <- sapply(spc, getElement, name = "P")
   
+  print(length(powerK))
   # Averaging to get multitaper statistic
-  # Note that we multiply by deltat * oversample
-  out <- data.frame( freq = freq, P = rowMeans(Pall) )
+  Sf <- rowMeans(powerK)
+  
+  # Adaptive weighting
+  if(!is.null(niterations)){
+    for(i in 1:niterations){
+      # Weighting using local "signal" and broad-band "noise"
+      dK <- Sf/(Sf%*%t(lambdaK) + (1 - lambdaK)*var(y))
+      # Spectrum Estimate
+      Sf <- rowMeans(dK^2*lambdaK*powerK)/sum(dK^2*lambdaK)
+    }
+  }
+  out <- data.frame(freq = freq, P = Sf)
+  
+  if(Ftest){
+    ftestRes <- Ftestfun(t, k, preprocess = TRUE, tpIVal = tpIVal, freq = freq, powerK = powerK)
+    attr(out, "Ftest") <- ftestRes
+  }
+  
   attr( out, "w" ) <- w
   attr( out, "k" ) <- k
   attr( out, "n" ) <- N
   attr( out, "tRange" ) <- tRange
   attr( out, "os" ) <- os
   attr( out, "subtract.mean" ) <- subtract.mean
-out
+  out
+}
+
+PspecMT <- function( t, x, os = 1, tRange = range(t), w, k, tpI = NULL, subtract.mean = TRUE, niterations = NULL, Ftest = TRUE){
+  # Number of samples
+  N <- length(t)
+  # Total time window
+  T <- diff(tRange)
+  
+  if(subtract.mean) x <- x - mean(x)
+  
+  # Compute Interpolated DPSS tapers if not provided
+  if(is.null(tpI)){
+    dpssIN <- dpssApproxFunEigen(t, w, k, tRange = tRange, returnEigenvalues = TRUE)
+    tpI <- dpssIN$tpI
+    lambdaK <- dpssIN$eigen    # Eigenvalues are required only if adaptive weighting is performed
+  }
+  
+  tpIVal <- lapply(tpI, do.call, list(t))
+  
+  # The tapers * data
+  pf <- function(taper, t, x, T, dt){
+    taper/sum(taper^2) * x * sqrt(T/dt) 
+  }
+  taperedData <- lapply(tpIVal, pf, t = t, x = x, T = T, dt = T/N)
+  
+  # Redefine nfft function
+  nfft.2 <- function(x, t, os, setN, tRange) nfft(t, x, os = os, setN = TRUE, tRange = tRange)
+  # Compute spectra
+  spc <- lapply(taperedData, nfft.2, t = t, os = os, tRange = tRange)
+  # Frequency and Power
+  freq <- seq(from = 0, by = 1/os/diff(tRange), along.with = spc[[1]])
+  powerK <- sapply(spc, function(x) abs(x)^2)
+  
+  # Averaging to get multitaper statistic
+  # Note that we multiply by deltat * oversample
+  Sf <- rowMeans(powerK) * 0.5 * T / N / N
+  
+  if(!is.null(niterations)){
+    for(i in 1:niterations){
+      dK <- sqrt(lambdaK)*Sf/(lambdaK*Sf + (1 - lambdaK)*var(x))
+      Sf <- rowMeans(dK^2*powerK)/sum(dK^2)
+    }
+  }
+  out <- data.frame(freq = freq, P = Sf)
+  
+  if(Ftest){
+    ftestRes <- Ftestfun(t, k, preprocess = TRUE, tpIVal = tpIVal, freq = freq, powerK = powerK)
+    attr(out, "Ftest") <- ftestRes
+  }
+  
+  attr( out, "w" ) <- w
+  attr( out, "k" ) <- k
+  attr( out, "n" ) <- N
+  attr( out, "tRange" ) <- tRange
+  attr( out, "os" ) <- os
+  attr( out, "subtract.mean" ) <- subtract.mean
+  out
+}
+
+## Slow Non-uniform Discrete Fourier Transform (NUDFT)
+nudft <- function(z, t, inverse=FALSE) {
+  N <- length(t)
+  if(N == 0) return(z)
+  T <- diff(range(t))
+  ff <- (if(inverse) 1 else -1) * 1i * 2*pi/T * (t - t[1])
+  vapply(1:N, function(h) sum(z * exp(ff*(h-1))), complex(1))
+}
+
+
+NUspecMT <- function(t, x, os = 1, tRange = range(t), w, k, tpI = NULL, subtract.mean = TRUE, niterations = NULL, Ftest = TRUE){
+  # Number of samples
+  N <- length(t)
+  # Total time window
+  T <- diff(tRange)
+  
+  if(subtract.mean) x <- x - mean(x)
+  
+  # Compute Interpolated DPSS tapers if not provided
+  if( is.null(tpI) ){
+    dpssIN <- dpssApproxFunEigen(t, w, k, tRange = tRange, returnEigenvalues = TRUE)
+    tpI <- dpssIN$tpI
+    lambdaK <- dpssIN$eigen
+  }
+  
+  tpIVal <- lapply(tpI, do.call, list(t))
+  
+  # The tapers * data
+  pf <- function(taper, t, x, T, dt){
+    taper/sum(taper^2) * x * sqrt(T/dt) 
+  }
+  taperedData <- lapply(tpIVal, pf, t = t, x = x, T = T, dt = T/N)
+  
+  # Compute spectra
+  spc <- lapply(taperedData, nudft, t = t)
+  # Frequency and Power
+  freq <- seq(from = 0, by = 1/os/diff(tRange), along.with = spc[[1]])
+  powerK <- sapply(spc, function(x) abs(x)^2)
+  
+  # Averaging to get multitaper statistic
+  # Note that we multiply by deltat * oversample
+  Sf <- rowMeans(powerK) * 0.5 * T / N / N
+  
+  if(!is.null(niterations)){
+    for(i in 1:niterations){
+      dK <- sqrt(lambdaK)*Sf/(lambdaK*Sf + (1 - lambdaK)*var(x))
+      Sf <- rowMeans(dK^2*powerK)/sum(dK^2)
+    }
+  }
+  out <- data.frame(freq = freq, P = Sf)
+  
+  if(Ftest){
+    ftestRes <- Ftestfun(t, k, preprocess = TRUE, tpIVal = tpIVal, freq = freq, powerK = powerK)
+    attr(out, "Ftest") <- ftestRes
+  }
+  
+  attr( out, "w" ) <- w
+  attr( out, "k" ) <- k
+  attr( out, "n" ) <- N
+  attr( out, "tRange" ) <- tRange
+  attr( out, "os" ) <- os
+  attr( out, "subtract.mean" ) <- subtract.mean
+  out
 }
 
 
